797630c95844fb3f056e840b97b4f48c87986891
Tyrone Woody <bobbywoody7@gmail.com>
2023-01-29 00:18:07 -0700

    firebase config file add

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import "./index.css";
+import { app, analytics } from "./firebaseConfig";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <App firebase={app} analytics={analytics} />
     </BrowserRouter>
   </React.StrictMode>
 );
