11330c4338d8b944e1af8cfa92b754c6bf633f38
Tyrone Woody <bobbywoody7@gmail.com>
2023-02-06 18:25:35 -0700

    demo

--- src/pages/Demo/Demo.jsx
+++ src/pages/Demo/Demo.jsx
@@ -29,48 +29,44 @@ export const Demo = () => {
   return (
     <div>
       <Navbar />{" "}
-      <div>
-        <div className="demo__main">
-          <div className="demo__container">
-            <h2 className="demo__title">
-              See how Reel Talent can improve your hiring
-            </h2>
-            <p className="demo__pg">
-              From recruiting candidates to onboarding new team members, Reel
-              Talent gives your company everything you need to be great at
-              hiring.
-            </p>
-            <h4 className="demo__booking">Book a session with us to:</h4>
-            <ul className="demo__list">
-              <li className="">Get an overview of the Reel Talent platform</li>
-              <li className="">Discuss your goals with a product expert</li>
-              <li className="">Receieve pricing details based on your needs</li>
-            </ul>
-            <p className="demo__pg">
-              {" "}
-              Once you complete the request form, a Reel Talent team member will
-              reach out within 24 business hours.
-            </p>
-          </div>
-          <div className="calendly__container">
+      <main className="demo__main">
+        <div className="demo__container">
+          <h2 className="demo__title">
+            See how Reel Talent can improve your hiring
+          </h2>
+          <p className="demo__pg">
+            From recruiting candidates to onboarding new team members, Reel
+            Talent gives your company everything you need to be great at hiring.
+          </p>
+          <h4 className="demo__booking">Book a session with us to:</h4>
+          <ul className="demo__list">
+            <li className="">Get an overview of the Reel Talent platform</li>
+            <li className="">Discuss your goals with a product expert</li>
+            <li className="">Receieve pricing details based on your needs</li>
+          </ul>
+          <p className="demo__pg">
             {" "}
-            <Calendly />
-          </div>
+            Once you complete the request form, a Reel Talent team member will
+            reach out within 24 business hours.
+          </p>
         </div>
-
-        {/* Contact box */}
-        {/* <section className="contact__main"> */}
-        {/* <div className="contact__container">
-            {" "}
-            <h2 className="contact__heading">Contact us</h2>
-            <p className="contact__pg">
-              Reach out to us at Reel Talent to transform your recruitment
-              process and build a more engaged workforce. With our expertise,
-              accelerate your hiring and connect with the best talent available.
-              Let's work together to build a strong foundation for your
-              company's success.
-            </p>
-          </div> */}
+        <div className="calendly__container">
+          {" "}
+          <Calendly />
+        </div>
+      </main>
+      {/* Contact box */}
+      <section className="contact__main">
+        <div className="contact__container">
+          {" "}
+          <h2 className="contact__heading">Contact us</h2>
+          <p className="contact__pg">
+            Reach out to us at Reel Talent to transform your recruitment process
+            and build a more engaged workforce. With our expertise, accelerate
+            your hiring and connect with the best talent available. Let's work
+            together to build a strong foundation for your company's success.
+          </p>
+        </div>
 
         {/* Form */}
         {/* <form onSubmit={handleSubmit}>
@@ -127,10 +123,8 @@ export const Demo = () => {
               Submit
             </button>
           </form> */}
-        {/* </section> */}
-
-        <SiteFooter />
-      </div>
+      </section>
+      <SiteFooter />
     </div>
   );
 };
