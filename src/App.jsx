4acf6e329ae1ea12893b6b78f3b503c7521254d2
Bobby Tyrone Woody <bobbywoody7@gmail.com>
2023-05-07 10:41:52 -0600

    Update document title to match page content

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,40 @@ import ErrorBoundary from "./helpers/ErrorBoundary";
 import { Navbar } from "./components/Navbar";
 import { SiteFooter } from "./components/SiteFooter";
 // Packages
-import { Routes } from "react-router-dom";
-import React from "react";
+import { Routes, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 export const App = () => {
+  const { pathname } = useLocation();
+  console.log(pathname);
+
+  useEffect(() => {
+    let title;
+    switch (pathname) {
+      case "/blog":
+        title =
+          "Reel Talent Blog - Insights and Resources for Hiring and Recruitment";
+        break;
+      case "/demo":
+        title = "Reel Talent Demo - Try Our Recruiting Solutions Now";
+        break;
+      case "/about":
+        title = "About Us - Reel Talent";
+        break;
+      case "/solutions":
+        title = "Reel Talent Solutions: Empowering Your Recruitment Strategies";
+        break;
+      case "/404":
+        title = "Page Not Found - Reel Talent";
+        break;
+      default:
+        title = "Reel Talent: Your Expert Recruiting Partner";
+        break;
+    }
+
+    document.title = title;
+  }, [pathname]);
+
   return (
     <>
       <Navbar />
