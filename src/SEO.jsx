9096424a3311f294c2a2288dace5c34af597f65f
Bobby Tyrone Woody <bobbywoody7@gmail.com>
2023-05-09 21:38:16 -0600

    seo meta tags

--- src/SEO.jsx
+++ src/SEO.jsx
@@ -3,7 +3,6 @@ import { Helmet } from "react-helmet";
 export const SEO = () => {
   return (
     <Helmet>
-      <title>Reel Talent: Your Expert Recruiting Partner</title>
       <meta
         name="description"
         content="Reel Talent provides comprehensive external recruiting services to help businesses fill job roles with top talent. Partner with us today."
@@ -11,6 +10,14 @@ export const SEO = () => {
       <meta
         name="keywords"
         content="recruiting, external recruiting, job placement, talent acquisition, Reel Talent"
+      />
+      <meta name="author" content="Reel Talent" />
+      <meta name="robots" content="index,follow" />
+      <meta name="location" content="Salt Lake City, Utah" />
+      <meta name="industry" content="Recruiting" />
+      <meta
+        name="services"
+        content="External Recruiting, Job Placement, Talent Acquisition"
       />
     </Helmet>
   );
